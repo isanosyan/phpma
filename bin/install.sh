69b7bb802f7431dce4708190a4b51a39bc353591
Ilya Sanosyan <ilya.a.sanosyan@gmail.com>
2012-08-26 18:16:31 +0300

    bool vars can be configured

--- bin/install.sh
+++ bin/install.sh
@@ -5,12 +5,11 @@ CONFIG_PATH=$DIR"/../conf/";
 # loading config
 source $CONFIG_PATH"config";
 
+# init local config file
 LOCAL_CONFIG_FILE=$CONFIG_PATH"local"
-
 if [ -f $CONFIG_PATH"local" ]; then
     mv $LOCAL_CONFIG_FILE $LOCAL_CONFIG_FILE"-prev"
 fi
-
 cp $LOCAL_CONFIG_FILE"-dist" $LOCAL_CONFIG_FILE
 
 function configure () {
@@ -20,13 +19,36 @@ function configure () {
 
     DEFAULT_NAME="DEFAULT_"$VAR;
 
-    read -p "Enter your $MESSAGE ("${!DEFAULT_NAME}" by default): " VALUE
-    if [ "$VALUE" == "" ]; then
-        VALUE=${!DEFAULT_NAME};
-    fi
+    case $TYPE in
+        string )
+            read -p "Enter your $MESSAGE ("${!DEFAULT_NAME}" by default): " VALUE
+            if [ "$VALUE" == "" ]; then
+                VALUE=${!DEFAULT_NAME};
+            fi
+            ;;
+        bool )
+            while true; do
+                read -p "$MESSAGE (y/n) " VALUE
+                case $VALUE in
+                    [Yy]* ) 
+                        VALUE="true";
+                        break;
+                        ;;
+                    [Nn]* )
+                        VALUE="false";
+                        break;
+                        ;;
+                esac
+            done
+            ;;
+        * )
+            echo "*";
+            ;;
+    esac
 
     sed -i "" "s|__REPLACE_"$VAR"|"$VALUE"|g" $LOCAL_CONFIG_FILE
 }
 
 configure string CODEBASE_PATH "local HuffPost codebase path";
 configure string GIT_BRANCH "git branch";
+configure bool GIT_PULL_BEFORE_RUN "Pull before analysis?";
